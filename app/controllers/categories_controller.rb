8cd68ac2bae584d848ccf218ec9c15af227fd5ca
Jesus Castilla <jesus.jcg8@gmail.com>
2022-05-18 22:20:14 -0500

    Categories controller: actions completed

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,3 +1,39 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
 
+  def show
+    @categorie = Category.find(params[:id])
+  end
+
+  def new
+    @categorie = Category.new
+  end
+
+  def create
+    @categorie = Category.create(categories_params)
+    redirect_to @categories_path
+  end
+
+  def edit
+    @categorie = Category.find(params[:id])
+  end
+
+  def update
+    @categorie.update(categories_params)
+    redirect_to categorie_path(@categorie)
+  end
+
+  def destroy
+    @categorie = Category.find(params[:id])
+    @categorie.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def categories_params
+    params.require(:categories).permit(:name)
+  end
 end
